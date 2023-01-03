2e7a08df4df3f7bba569f67834811c0ae7fda7bb
David Vandensteen <dvandensteen@gmail.com>
2023-01-03 13:16:07 +0100

    Update : test script

--- test/remote-midi-client.js
+++ test/remote-midi-client.js
@@ -1,43 +1,99 @@
+import { emitKeypressEvents } from 'readline';
 import { rMidiClient } from '#src/remote-midi';
 
-const syncWait = (ms) => {
-  const end = Date.now() + ms;
-  // eslint-disable-next-line no-unused-expressions
-  while (Date.now() < end) () => 0;
-};
+const { log } = console;
+
+const velocity = 127;
+const channel = 0;
+const baseNote = 96;
 
 const client = rMidiClient({ host: '127.0.0.1', port: 7070 });
 client.start();
 
-const velocity = 127;
-const channel = 0;
+log('press a z e r t y u i to send midi note C D E F G A B C ');
 
-client.send('cc', {
-  controller: 40,
-  value: 64,
-});
-
-setInterval(() => {
-  client.send('noteon', {
-    note: 100,
+const sendNote = (midiSender, note) => {
+  midiSender('noteon', {
+    note,
     velocity,
     channel,
   });
-}, 500);
+  setTimeout(() => {
+    midiSender('noteoff', {
+      note,
+      velocity,
+      channel,
+    });
+  }, 300);
+};
 
-/*
+const keyboard = () => {
+  const { stdin } = process;
+  emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
 
-for (let i = 100; i < 127; i += 1) {
-  client.send('noteon', {
-    note: i,
-    velocity,
-    channel,
+  stdin.on('keypress', (str, keypressing) => {
+    if (keypressing.ctrl && keypressing.name === 'c') process.exit();
+    else {
+      log(keypressing);
+      switch (keypressing.sequence) {
+        case 'a':
+          sendNote(client.send.bind(client), baseNote);
+          break;
+
+        case 'é':
+          sendNote(client.send.bind(client), baseNote + 1);
+          break;
+
+        case 'z':
+          sendNote(client.send.bind(client), baseNote + 2);
+          break;
+
+        case '"':
+          sendNote(client.send.bind(client), baseNote + 3);
+          break;
+
+        case 'e':
+          sendNote(client.send.bind(client), baseNote + 4);
+          break;
+
+        case 'r':
+          sendNote(client.send.bind(client), baseNote + 5);
+          break;
+
+        case '(':
+          sendNote(client.send.bind(client), baseNote + 6);
+          break;
+
+        case 't':
+          sendNote(client.send.bind(client), baseNote + 7);
+          break;
+
+        case '-':
+          sendNote(client.send.bind(client), baseNote + 8);
+          break;
+
+        case 'y':
+          sendNote(client.send.bind(client), baseNote + 9);
+          break;
+
+        case 'è':
+          sendNote(client.send.bind(client), baseNote + 10);
+          break;
+
+        case 'u':
+          sendNote(client.send.bind(client), baseNote + 11);
+          break;
+
+        case 'i':
+          sendNote(client.send.bind(client), baseNote + 12);
+          break;
+
+        default:
+          break;
+      }
+    }
   });
+};
 
-  client.send('noteoff', {
-    note: i,
-    velocity,
-    channel,
-  });
-}
-*/
+keyboard();
