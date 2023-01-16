5ed657dbed6e117043d901be79ad1b0ef51019ab
David Vandensteen <dvandensteen@gmail.com>
2023-01-16 18:15:01 +0100

    Add : bidirectional test script

--- test/remote-midi-server.js
+++ test/remote-midi-server.js
@@ -1,6 +1,6 @@
 import { rMidiServer } from '#src/remote-midi';
 
 const server = rMidiServer({
-  host: '0.0.0.0', port: 7070, midiOutputDeviceName: 'vmidi-out', midiInputDeviceName: 'vmidi-in',
+  host: '0.0.0.0', port: 7070, midiOutputDeviceName: 'vmidi-out',
 });
 server.start();
