c8758bb34b755037976e42c0ab46551dbfa2ba57
David Vandensteen <dvandensteen@gmail.com>
2023-01-16 13:34:40 +0100

    Add : midi client cc test

--- test/remote-midi-server.js
+++ test/remote-midi-server.js
@@ -1,6 +1,6 @@
 import { rMidiServer } from '#src/remote-midi';
 
 const server = rMidiServer({
-  host: '127.0.0.1', port: 7070, midiOutputDeviceName: 'vmidi-out', midiInputDeviceName: 'vmidi-in',
+  host: '0.0.0.0', port: 7070, midiOutputDeviceName: 'vmidi-out', midiInputDeviceName: 'vmidi-in',
 });
 server.start();
