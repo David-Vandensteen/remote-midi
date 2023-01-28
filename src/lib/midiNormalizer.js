a2eaf5b3b2c6569c4f5ac551e45a996f6802fe16
David Vandensteen <dvandensteen@gmail.com>
2023-01-28 17:28:38 +0100

    Add : midi channel normalizer method

--- src/lib/midiNormalizer.js
+++ src/lib/midiNormalizer.js
@@ -25,6 +25,12 @@ export default class MidiNormalizer {
     return controller;
   }
 
+  static channel(channel) {
+    if (channel >= 15) return 15;
+    if (channel <= 0) return 0;
+    return channel;
+  }
+
   static value(value) {
     if (value >= 127) return 127;
     if (value <= 0) return 0;
