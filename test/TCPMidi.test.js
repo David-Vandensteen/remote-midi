6fc0b377fa70e2dacd76ef99ef4fa73001f9be6f
david-vandensteen <dvandensteen@gmail.com>
2023-04-22 16:20:34 +0200

    Add : TCPMidi test

---
+++ test/TCPMidi.test.js
@@ -0,0 +1,27 @@
+import { TCPMidi } from '#src/lib/tcpMidi';
+import { TCPMessage } from '#src/lib/tcpMessage';
+import { expect } from 'chai';
+import mocha from 'mocha';
+
+const describe = mocha.describe;
+const it = mocha.it;
+const after = mocha.after;
+
+describe('TCPMidi', () => {
+  describe('#send', () => {
+    it('should send a message with the correct type', (done) => {
+      const tcpMidi = new TCPMidi('127.0.0.1', 1234);
+      const message = { id: 1 };
+      const expectedMessage = { _type: 'test', id: 1 };
+
+      tcpMidi.write = (data) => {
+        const result = TCPMessage.decode(data);
+        expect(result).to.deep.equal([expectedMessage]);
+        done();
+      };
+
+      tcpMidi.send('test', message);
+      tcpMidi.end();
+    });
+  });
+});
